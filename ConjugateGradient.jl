5cb30d63ac713f59dc7127085e0216f15bcc004f
amel123amel <64417413+amel123amel@users.noreply.github.com>
2022-04-08 15:36:52 +0200

    Create ConjugateGradient.jl

---
+++ ConjugateGradient.jl
@@ -0,0 +1,24 @@
+
+function conjugateGradientMethod(A,b,x_n)
+r = b-A*x_n
+p = r
+
+        for i = 1:length(b)
+                alfa = (r'*r)/(p'*A*p)
+                x_new = x_n+alfa*p
+                r_new = r-alfa*A*p
+                beta = (r_new'*r_new)/(r'*r)
+
+                display(x_new)
+                if(sqrt((r'*r))<10^-10)
+
+                        display(["I : " i])
+                        break;
+                end
+                p_new = r_new+beta*p
+                p = p_new
+                r = r_new
+                x_n = x_new
+        end
+end 
+
